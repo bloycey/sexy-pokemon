bc14f511fe5d0ad2913899fa3af24cc0aef31da8
Andrew Bloyce <bloyce1990@gmail.com>
2020-12-15 16:17:28 +1000

    who knows

--- functions/updateRanking/updateRanking.js
+++ functions/updateRanking/updateRanking.js
@@ -1,4 +1,41 @@
 const EloRating = require("elo-rating");
+const MongoClient = require("mongodb").MongoClient;
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+const updateRankings = async (ids, winner) => {
+	await client.connect();
+	const db = await client.db("rankings");
+	const collection = await db.collection("rankings");
+
+	const [pokeZeroOriginal] = await collection
+	.find({ id: ids[0] })
+	.toArray();
+const [pokeOneOriginal] = await collection
+	.find({ id: ids[1] })
+	.toArray();
+
+	const sexyResult = EloRating.calculate(
+		pokeZeroOriginal.ranking || 1000,
+		pokeOneOriginal.ranking || 1000,
+		winner === ids[0]
+	);
+
+	const pokeZeroFinal = sexyResult.playerRating;
+	const pokeOneFinal = sexyResult.opponentRating;
+
+	collection.updateOne(
+		{ id: ids[0] },
+		{ $set: { ranking: pokeZeroFinal } }
+	);
+	collection.updateOne(
+		{ id: ids[1] },
+		{ $set: { ranking: pokeOneFinal } }
+	);
+	client.close();
+	return `${ids[0]} changed from ${pokeZeroOriginal.ranking} to ${pokeZeroFinal}. ${ids[1]} changed from ${pokeOneOriginal.ranking} to ${pokeOneFinal}`
+
+};
 
 exports.handler = async function (event, context) {
 	const body = JSON.parse(event.body);
@@ -8,45 +45,10 @@ exports.handler = async function (event, context) {
 	const ids = body.pokemon.map((pokemon) => pokemon.id);
 	const winner = body.winner;
 
-	const MongoClient = require("mongodb").MongoClient;
-	const uri = process.env.DB_URI;
-	const client = new MongoClient(uri, { useNewUrlParser: true });
-
-	client.connect(async (err) => {
-		const db = await client.db("rankings");
-		const collection = await db.collection("rankings");
-		const [pokeZeroOriginal] = await collection
-			.find({ id: ids[0] })
-			.toArray();
-		const [pokeOneOriginal] = await collection
-			.find({ id: ids[1] })
-			.toArray();
-
-		const sexyResult = EloRating.calculate(
-			pokeZeroOriginal.ranking || 1000,
-			pokeOneOriginal.ranking || 1000,
-			winner === ids[0]
-		);
-
-		const pokeZeroFinal = sexyResult.playerRating;
-		const pokeOneFinal = sexyResult.opponentRating;
-
-		collection.updateOne(
-			{ id: ids[0] },
-			{ $set: { ranking: pokeZeroFinal } }
-		);
-		collection.updateOne(
-			{ id: ids[1] },
-			{ $set: { ranking: pokeOneFinal } }
-		);
-		console.log(
-			`${pokeZeroName} (${ids[0]}) changed from ${pokeZeroOriginal.ranking} to ${pokeZeroFinal}. ${pokeOneName} (${ids[1]}) changed from ${pokeOneOriginal.ranking} to ${pokeOneFinal}`
-		);
-		client.close();
-	});
+	const data = await updateRankings(ids, winner).catch(err => console.log(err));
 
 	return {
 		statusCode: 200,
-		body: JSON.stringify("Success"),
+		body: JSON.stringify(data),
 	};
 };
